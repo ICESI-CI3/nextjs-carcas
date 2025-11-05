651e14785f5ee800c8b7af16b8e1dc8ede8eaa5b
AIejoCastro <alejocastro20@hotmail.com>
2025-11-05 07:45:58 -0500

    Landing Page

--- e2e/example.spec.ts
+++ e2e/example.spec.ts
@@ -2,5 +2,5 @@ import { test, expect } from '@playwright/test';
 
 test('home has title', async ({ page }) => {
   await page.goto('/');
-  await expect(page.locator('h1')).toHaveText(/Biblioteca/);
+  await expect(page.locator('h1')).toHaveText(/Bienvenido a BiblioIcesi/);
 });
